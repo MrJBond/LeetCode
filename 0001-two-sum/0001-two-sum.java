5bcdc4fa351801887b9aa310c8861df4794a436b
MrJBond <143087645+MrJBond@users.noreply.github.com>
2023-08-25 14:47:31 +0300

    Time: 4 ms (66.10%), Space: 43.4 MB (89.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,5 +1,9 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    
+    
+    // brute force
+    
+    /*public int[] twoSum(int[] nums, int target) {
        int[] res = new int[2];
        for(int i=0; i<nums.length; i++){
            for(int j=i+1; j<nums.length; j++){
@@ -10,5 +14,30 @@ class Solution {
            }
        }
        return res; 
+    }*/
+    
+    // using HashSet
+    
+        // using HashMap
+        
+    public static int[] twoSum(int[] nums, int target) {
+        
+        int[] res = {0,0};
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        for(int i = 0; i<nums.length; i++){
+            
+            if(map.containsKey(target - nums[i])){
+                Integer el = map.get(target - nums[i]);
+            
+                res[0] = i;
+            
+                res[1] = el;
+            }
+            
+            map.put(nums[i],i);
+        }
+        return res;
     }
 }
