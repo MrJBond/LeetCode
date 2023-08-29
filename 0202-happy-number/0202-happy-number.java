70ffcd1c66a30a7244c94c2226d756b90623075b
MrJBond <143087645+MrJBond@users.noreply.github.com>
2023-08-29 14:57:47 +0300

    Time: 1 ms (87.76%), Space: 39.4 MB (76.33%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    int sumOfSq(int n){
+        int sum = 0;
+        
+        while(n > 0){
+            sum += (n%10)*(n%10);
+            n = n/10;
+        }
+        return sum;
+    }
+
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<Integer> ();
+
+        while(n != 1 && !set.contains(n)){
+            set.add(n);
+            n = sumOfSq(n);
+        }
+        return n == 1;     
+    }
+}
