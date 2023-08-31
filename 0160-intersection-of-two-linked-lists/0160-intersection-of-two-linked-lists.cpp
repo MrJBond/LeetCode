1413f3071257e090e5361d24de14cc950884a16c
MrJBond <143087645+MrJBond@users.noreply.github.com>
2023-08-31 14:18:15 +0300

    Time: 41 ms (63.44%), Space: 14.5 MB (91.34%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -11,7 +11,7 @@ public:
 
     // brute force solution (two loops)
 
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    /*ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ListNode *wp1 = headA;
         ListNode *res = NULL;
         while(wp1!=NULL){
@@ -25,6 +25,72 @@ public:
             wp1=wp1->next;
         }
         return res;
+    }*/
+
+
+    // O(n+m)
+
+    int len(ListNode * l){
+        ListNode * wp = l;
+        int count = 0;
+
+        while(wp != NULL){
+            count++;
+            wp = wp->next;
+        }
+        return count;
     }
 
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+         ListNode *wp1 = headA;
+         ListNode *wp2 = headB;
+        
+        // there is no intersection
+        if(wp1 == NULL || wp2 == NULL){
+             return NULL;
+         }
+        
+        // find difference between lengthes
+         int dif = abs(len(headA) - len(headB)); 
+         ListNode* start = NULL;
+         ListNode* start2 = NULL;
+
+        // adjust the position of "start" pointer 
+        // by skipping nodes which make difference between lengthes
+        if(len(headA) > len(headB)){
+            start2 = headB;
+            start = headA;
+            int count = dif;
+
+            while(count != 0){
+                start = start->next;
+                count--;
+            }
+        }
+        else if(len(headA) < len(headB)){
+            start2 = headA;
+            start = headB;
+            int count = dif;
+
+            while(count != 0){
+                start = start->next;
+                count--;
+            }
+        }
+        else{
+           start = headA; 
+           start2 = headB;
+        }
+
+        // find intersection
+         while(start != NULL && start2 != NULL){
+             if(start == start2){
+                 return start;
+             }
+             start = start->next;
+             start2 = start2->next;
+         }
+         return NULL;
+        
+}
 };
