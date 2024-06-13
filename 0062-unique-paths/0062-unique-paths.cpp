0c6b925d414c311b70d235a5a57eb1deb46d55a1
Serhii <143087645+MrJBond@users.noreply.github.com>
2024-06-13 14:30:11 +0300

    Time: 0 ms (100.00%), Space: 7.7 MB (16.88%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
-struct pair_hash { // To store the pair as a key
+
+// first solution
+
+/*struct pair_hash { // To store the pair as a key
 	template<class T1, class T2>
 	size_t operator()(const pair<T1, T2>& p) const{
 		auto h1 = hash<T1>{}(p.first);
@@ -22,5 +25,24 @@ int uniquePaths(int m, int n) {
 	int res = uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
 	mem[make_pair(m, n)] = res;
 	return res;
+}*/
+
+// second solution
+
+int helper(int n, int m, vector<vector<int>>& dp){
+    if(dp[n][m] != -1){
+        return dp[n][m];
+    }
+    if(n == 1 || m == 1){
+        return 1;
+    }
+    int res = helper(n-1, m, dp) + helper(n, m-1, dp);
+    dp[n][m] = res;
+    return res;
 }
+int uniquePaths(int m, int n) {
+    vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+    return helper(n, m, dp);    
+}
+
 };
