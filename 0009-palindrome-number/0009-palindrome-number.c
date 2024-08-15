e7950df9c39bc17ab6496baeedd530ac68ed2a4c
Serhii <143087645+MrJBond@users.noreply.github.com>
2024-08-15 08:01:39 +0300

    Time: 13 ms (21.63%), Space: 8.1 MB (28.27%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,13 @@
+
+bool isPalindrome(int x) {
+    int n = x;
+    if(n < 0){return false;}
+    if(n < 10){return true;}
+    long long reverse = 0;
+    while(x >= 10){
+        reverse = reverse * 10 + (x % 10);
+        x /= 10;
+    }
+    reverse = reverse * 10 + x; // One more number
+    return reverse == n; 
+}
