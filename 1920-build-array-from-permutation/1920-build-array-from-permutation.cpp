c5b46de95a8f10b8f6dc9c5481e61c97158c4341
Serhii <143087645+MrJBond@users.noreply.github.com>
2024-08-20 08:16:49 +0300

    Time: 4 ms (94.66%), Space: 18.8 MB (75.30%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         vector<int> ans(n, 0);
         for(int i = 0; i<n; ++i){
-            ans[i] = nums[nums[i]];
+            ans[i] = nums[nums[i]] ;
         }
         return ans;
     }
